03896be35963a67d09c89498c22a744a5bd2e2ea
Raji Nurudeen <orizon.bot21@gmail.com>
2024-03-22 17:50:39 +0100

    base model updated

--- models/base_model.py
+++ models/base_model.py
@@ -11,12 +11,25 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """ Class Constructor """
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
     
     def __str__(self):
         """ Method to print the string """
         return "[{}] ({}) {}".format(self.__class__.__name__,
                 self.id, self.__dict__)
+
+    def save(self):
+        """ Updates the attributes - updated_at """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """ Returns a dictionary for all the keys/values of the dict """
+        dict_copy = self.__dict__.copy()
+
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+
+        return dict_copy
