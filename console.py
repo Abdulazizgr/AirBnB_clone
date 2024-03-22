da26333fe234c3e1295fd9e98f688a0be7e593ce
Raji Nurudeen <orizon.bot21@gmail.com>
2024-03-22 22:52:17 +0100

    Console 0.0.1

--- console.py
+++ console.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+    Entry point of the command interpreter
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+        The class inheriting the cmd module
+    """
+    prompt = "(hbnb) "
+
+    def emptyline(self):
+        """ overrides an empty command """
+        return
+
+    def do_quit(self, line):
+        """ Quit command to exit the pogram """
+        exit()
+
+    def do_EOF(self, line):
+        """ End of File, Ctrl+C && Ctrl+D """
+        return True
+
+    def postloop(self):
+        """ Overrides the end of the loop """
+        print()
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
