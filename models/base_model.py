d3cdf1d27cb7ddf47194c66fed1185fc9dd4f541
Abdulazizgr <abdulaizisa579@gmail.com>
2024-03-22 20:49:12 +0300

    the base model class

---
+++ models/base_model.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """
+    Base class for models with common attributes and methods.
+
+    Attributes:
+        id (str): Unique identifier for the model.
+        created_at (datetime): Timestamp indicating the creation time of the model.
+        updated_at (datetime): Timestamp indicating the last update time of the model.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of the BaseModel.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    value = datetime.fromisoformat(value)
+                elif key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        Returns a string representation of the BaseModel.
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the 'updated_at' attribute with the current timestamp.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns:
+            dict: Dictionary representation of the BaseModel instance.
+        """
+        model_dict = self.__dict__.copy()
+        model_dict["__class__"] = self.__class__.__name__
+        model_dict["created_at"] = self.created_at.isoformat()
+        model_dict["updated_at"] = self.updated_at.isoformat()
+        return model_dict
