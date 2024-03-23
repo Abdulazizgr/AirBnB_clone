e054817ca44db4616de2d2f746b19415ccde54f9
Raji Nurudeen <orizon.bot21@gmail.com>
2024-03-23 11:34:15 +0100

    Console updated with do_create

--- console.py
+++ console.py
@@ -3,6 +3,9 @@
     Entry point of the command interpreter
 """
 import cmd
+import models
+import shlex
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -11,22 +14,64 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = "(hbnb) "
 
+    class_maps = {'BaseModel': BaseModel}
+
     def emptyline(self):
         """ overrides an empty command """
-        return
+        pass
+
+    def help_emptyline(self):
+        """ Documentation for the emptyline """
+        print("\n".join(["This prints an empty line",
+                         "It ensures nothing is executed \
+when empty line + ENTER is pressed", ]))
+
+    def do_create(self, line):
+        """ Creates a new instance of the BaseModel and save it """
+        args = shlex.split(line)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+
+        if class_name in HBNBCommand.class_maps:
+            create_instance = HBNBCommand.class_maps[class_name]()
+            create_instance.save()
+            print('{}'.format(create_instance.id))
+        else:
+            print("** class doesn't exist **")
+
+    def help_create(self):
+        """ Documentation for the create command """
+        print("It creates a instance of the BaseModel, save it \
+and use it print the id of the Basemodel")
 
     def do_quit(self, line):
         """ Quit command to exit the pogram """
         exit()
 
+    def help_quit(self):
+        """ Documentation for the quit command"""
+        print("Exit the program cleanly when quit is entered")
+
     def do_EOF(self, line):
         """ End of File, Ctrl+C && Ctrl+D """
         return True
+
+    def help_EOF(self):
+        """ Documentation for EOF command """
+        print("\n".join(["Ctrl+C: KeyboardInterrupt that exit program",
+                         "Ctrl+D: End the program"]))
 
     def postloop(self):
         """ Overrides the end of the loop """
         print()
 
+    def help_postloop(self):
+        """ Documentation for postloop command """
+        print(""" Ensures a newline after the program is exited """)
 
+    
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
