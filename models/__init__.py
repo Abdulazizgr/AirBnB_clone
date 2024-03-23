a7ee692253da2c5243224959189b4442b8102062
Raji Nurudeen <orizon.bot21@gmail.com>
2024-03-23 23:13:10 +0100

    base_model updated

---
+++ models/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+this is init file
+"""
+
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,8 @@
 
 import uuid
 from datetime import datetime
+import models
+
 
 class BaseModel:
     """
@@ -9,8 +11,10 @@ class BaseModel:
 
     Attributes:
         id (str): Unique identifier for the model.
-        created_at (datetime): Timestamp indicating the creation time of the model.
-        updated_at (datetime): Timestamp indicating the last update time of the model.
+        created_at (datetime): Timestamp indicating the creation
+        time of the model.
+        updated_at (datetime): Timestamp indicating the last update
+        time of the model.
     """
 
     def __init__(self, *args, **kwargs):
@@ -32,18 +36,21 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
         Returns a string representation of the BaseModel.
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                self.id, self.__dict__))
 
     def save(self):
         """
         Updates the 'updated_at' attribute with the current timestamp.
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
