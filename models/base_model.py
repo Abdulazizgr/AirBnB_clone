4d12cc4b6ba40a0e6622398dda2dbdb05cd8d811
Raji Nurudeen <orizon.bot21@gmail.com>
2024-03-24 15:21:32 +0100

    models updated

--- models/base_model.py
+++ models/base_model.py
@@ -36,7 +36,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -50,15 +50,17 @@ class BaseModel:
         Updates the 'updated_at' attribute with the current timestamp.
         """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """
         Returns:
             dict: Dictionary representation of the BaseModel instance.
         """
+
         model_dict = self.__dict__.copy()
         model_dict["__class__"] = self.__class__.__name__
-        model_dict["created_at"] = self.created_at.isoformat()
-        model_dict["updated_at"] = self.updated_at.isoformat()
+        model_dict["created_at"] = str(self.created_at.isoformat())
+        model_dict["updated_at"] = str(self.updated_at.isoformat())
+
         return model_dict
